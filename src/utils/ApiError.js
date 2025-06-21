58299bcb6381d003674b7c50dda5bde2ca1d7bfd
Rishabh <rishabhjangid89@gmail.com>
2025-06-21 22:38:36 +0530

    changes in the files of util

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,17 +1,24 @@
-class ApiError extends Error 
-{
+class ApiError extends Error {
     constructor(
         statusCode,
-        message="something went wrong",
-        error=[],
-        stack=""
+        message= "Something went wrong",
+        errors = [],
+        stack = ""
     ){
         super(message)
-        this.statusCode=statusCode
-        this.data=null
-        this.message=message
-        this.error=error
-        this.success=false
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
     }
 }
+
 export {ApiError}
